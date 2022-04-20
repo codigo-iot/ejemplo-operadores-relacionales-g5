d83f5c8f003662d41ffcb95d25f61a939b842a0b
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-04-20 09:52:40 -0500

    Circuito corregido

--- boton-led/boton-led.ino
+++ boton-led/boton-led.ino
@@ -9,8 +9,8 @@
 // Bibliotecas
 
 // Constantes
-const int BOTON = 4;
-const int LED = 2;
+const int BOTON = 2;
+const int LED = 4;
 
 // Variables
 int dato;
@@ -20,9 +20,13 @@ int dato;
 // Condiciones iniciales - Se ejecuta sólo una vez al energizar
 void setup() {// Inicio de void setup ()
   // Aquí va tu código
+  Serial.begin (115200);
+  Serial.println ("Inicio");
   pinMode (BOTON, INPUT_PULLUP);//Configurar el pin del boton como entrada //INPUT, OUTPUT
   pinMode (LED, OUTPUT); // Configurar el pin del led como salida de voltaje
-  digitalWrite (LED, LOW);// Empieza con el led Apagado //1, true, HIGH, 0, false, LOW
+  //digitalWrite (LED, LOW);// Empieza con el led Apagado //1, true, HIGH, 0, false, LOW
+
+  delay (2000);
 }// Fin de void setup
 
 // Cuerpo del programa - Se ejecuta constamente
@@ -30,6 +34,8 @@ void loop() {// Inicio de void loop
   // put your main code here, to run repeatedly:
   dato = digitalRead (BOTON); //Leer el pin del boton
   digitalWrite (LED, dato); //Prender o apagar el led
+  Serial.println (dato);
+  delay (200);
 
 }// Fin de void loop
 
